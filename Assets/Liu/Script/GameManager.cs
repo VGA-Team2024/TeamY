af307d0976d5b5615c5a4d82e25e702d83eb44d7
a0966891512@gmail.com <a0966891512@gmail.com>
2024-02-11 19:02:30 +0900

    [update] 進数1ｍ000ｋ000　STRING [add] ui view   ui  prefub

--- Assets/Liu/Script/GameManager.cs
+++ Assets/Liu/Script/GameManager.cs
@@ -9,11 +9,11 @@ public class GameManager : MonoBehaviour
 {
     /// <summary> point of game </summary>
     [SerializeField] private ulong _point = 1000;
-    [SerializeField] private List<Item> _items = new List<Item>();
     [SerializeField] private bool _timer = true;
     [SerializeField] private ulong _clickPoint = 2;
-    public static GameManager Instance {get; private set; }
-    public event Action OnPointChanged;    
+    public static GameManager Instance { get; private set; }
+    public event Action OnPointChanged;
+
     private void Awake()
     {
         if (Instance == null)
@@ -26,6 +26,7 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
     /// <summary>　Set Game Point　</summary>
     /// <param name="newPoint"> 計算した後の　ポイント　</param>
     public void SetPoint(ulong newPoint)
@@ -40,24 +41,56 @@ public class GameManager : MonoBehaviour
         {
             _point = newPoint;
             return;
-        } 
+        }
         else
         {
             newPoint = 10000000000000000000;
             Debug.LogError("LIULIU : 進数");
         }
+
         OnPointChanged?.Invoke();
     }
+
     /// <summary> Geter </summary>
     /// <returns> Game point </returns>
     public ulong GetPoint()
     {
         return _point;
     }
+
     /// <summary>  Click したらここ呼ぶ</summary>
     public void Click()
     {
-        SetPoint(GetPoint() + _clickPoint); 
+        SetPoint(GetPoint() + _clickPoint);
+    }
+
+    public ulong GetPointkata()
+    {
+        return _point;
+    }
+
+    /// <summary> ポイントを文字列として取得 </summary>
+    /// <returns> ポイントの文字列表現 </returns>
+    public string GetPointAsString()
+    {
+        string[] suffixes = {"k", "m", "b" ,"t","a","b","c","d"}; // サフィックスの配列
+
+        ulong tmp = _point;
+        string result = "";
+        for ( int i = 0 ; tmp > 0 ; i++)
+        {
+            if (tmp >= 1000)
+            {
+                result = suffixes[i] + (tmp % 1000).ToString("000") + result;
+                tmp /= 1000;
+            }
+            else
+            {
+                result = tmp + result;
+                tmp = 0;
+            }
+        }
+        return  result;
     }
 
     private IEnumerator count()
@@ -68,4 +101,3 @@ public class GameManager : MonoBehaviour
         }
     }
 }
-
