f872f9a5ea3aac925aea18c831f17a44faa1692b
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-02-27 21:01:46 +0900

    [add] スプライトを回転させる処理、ショップパネルを切り替える処理を追加。

---
+++ Assets/Script/UI/SwitchPanels.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SwitchPanels : MonoBehaviour
+{
+    /// <summary>施設</summary>
+    [SerializeField] GameObject _facilityPanel;
+    /// <summary>アップグレード</summary>
+    [SerializeField] GameObject _upgradePanel;
+    /// <summary>切り替え用の変数</summary>
+    bool _isUpgradePanelEnabled = false;
+    void Start()
+    {
+        // アップグレードパネルを無効化
+        _upgradePanel.SetActive(false);
+    }
+
+    public void Switch()
+    {
+        // 施設パネルが有効化されている場合
+        if(!_isUpgradePanelEnabled)
+        {   
+            _isUpgradePanelEnabled = true;
+            _facilityPanel.SetActive(false);
+            _upgradePanel.SetActive(true);
+        }
+        // アップグレードパネルが有効化されている場合
+        else
+        {
+            _isUpgradePanelEnabled = false;
+            _facilityPanel.SetActive(true);
+            _upgradePanel.SetActive(false);
+        }
+    }
+}
