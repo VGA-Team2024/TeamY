4e8431bcd490d2e70f98e63a30805c170a1da8d3
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-03-03 13:37:01 +0900

    [fix] 施設クラスのリファクタリング（ボタンの有効・無効化を設定するメソッド）

--- Assets/Script/Game/Shop/Facility.cs
+++ Assets/Script/Game/Shop/Facility.cs
@@ -33,22 +33,24 @@ public class Facility : MonoBehaviour
     Image _buttonImage;
 
     ///<summary>施設アイコンのイメージ</summary>
-    Image _iconImage;
+    [SerializeField] private Image _iconImage;
 
     ///<summary>テキストのイメージ</summary>
-    Image _textImage;
+    [SerializeField] private Image _textImage;
 
     ///<summary>施設名のテキスト</summary>
-    TextMeshProUGUI _nameText;
+    [SerializeField] private TextMeshProUGUI _nameText;
 
     ///<summary>施設価格のテキスト</summary>
-    TextMeshProUGUI _priceText;
+    [SerializeField] private TextMeshProUGUI _priceText;
 
     ///<summary>ゲーム管理クラス</summary>
     GameManager _gameManager;
     void Start()
     {
-        
+        // 初期化
+        SetUp();
+        // 
     }
     void Update()
     {
@@ -88,25 +90,53 @@ public class Facility : MonoBehaviour
     ///<param name="status">設定したいボタンの状態。0 = 非表示, 1 = 表示（半透明）, 2 = 表示（不透明）</param>
     void SetButtonEnablement(int status)
     {
-        Color Color = new Color(1, 1, 1, 0);
         switch (status)
         {
             case 0:
+                // イメージとテキストの色
+                Color invisibleColor = new Color(1, 1, 1, 0);
                 // ボタンを無効化
                 _button.enabled = false;
-                // 各イメージを透明化
-                
+                // 各イメージを非表示にする
+                _buttonImage.color = invisibleColor;
+                _iconImage.color = invisibleColor;
+                _textImage.color = invisibleColor;
+                // 各テキストを非表示にする
+                _nameText.color = invisibleColor;
+                _priceText.color = invisibleColor;
+
                 break;
 
             case 1:
+                // イメージとテキストの色
+                Color translucentImageColor = new Color(1, 1, 1, 0.25f);
+                Color translucentTextColor = new Color(0, 0, 0, 0.25f);
+                // ボタンを無効化
+                _button.enabled = false;
+                // 各イメージを非表示にする
+                _buttonImage.color = translucentImageColor;
+                _iconImage.color = translucentImageColor;
+                _textImage.color = translucentImageColor;
+                // 各テキストを非表示にする
+                _nameText.color = translucentTextColor;
+                _priceText.color = translucentTextColor;
 
                 break;
 
             case 2:
+                // イメージとテキストの色
+                Color opaqueImageColor = new Color(1, 1, 1, 1);
+                Color opaqueTextColor = new Color(0, 0, 0, 1);
+                // ボタンを無効化
+                _button.enabled = false;
+                // 各イメージを非表示にする
+                _buttonImage.color = opaqueImageColor;
+                _iconImage.color = opaqueImageColor;
+                _textImage.color = opaqueImageColor;
+                // 各テキストを非表示にする
+                _nameText.color = opaqueTextColor;
+                _priceText.color = opaqueTextColor;
 
-                break;
-
-            default: 
                 break;
         }
     }
